b64ff359dece32f2452fae309fa505c30b409684
msarwal345 <mani.sarwal2003@gmail.com>
2024-03-06 23:43:28 +0530

    Enhance flexibility: Allow custom names and locations for files

--- ImageHandling.java
+++ ImageHandling.java
@@ -12,8 +12,9 @@ import javax.imageio.ImageIO;
 /**
  * ImageHandling
  */
-public class ImageHandling {
 
+public class ImageHandling {
+    //Created a function to convert the URI to URL
     public static URL toURL(String s) throws MalformedURLException {
         URL x = null;
         try {
@@ -24,14 +25,61 @@ public class ImageHandling {
         return x;
     }
 
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+    //Created a Function to handle Conversiom
+    public void ImageConversion(BufferedImage image,int choice,String pathString,String fileName){
+        int flag=0;
+        String bmpFileName= "/"+fileName+".bmp";
+        String gifFileName= "/"+fileName+".gif";
+        String jpgFileName= "/"+fileName+".jpg";
+        String jpegFileName="/"+fileName+".jpeg";
+        String pngFileName= "/"+fileName+".png";
 
-        String userHome = System.getProperty("user.home");
-        System.out.println(userHome);
-        String documentsPath = userHome + File.separator + "Documents";
-        System.out.println(File.separator);
-        String downloadsPath = userHome + File.separator + "Downloads";
+        File dir = new File(pathString);
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        try {
+            if (choice == 1) {
+                ImageIO.write(image, "png", new File(dir.getPath() + pngFileName));
+            } 
+            else if (choice == 2) {
+                ImageIO.write(image, "gif", new File(dir.getPath() + gifFileName));
+            } 
+            else if (choice == 3) {
+                ImageIO.write(image, "bmp", new File(dir.getPath() + bmpFileName));
+            } 
+            else if (choice == 4) {
+                ImageIO.write(image, "jpg", new File(dir.getPath() + jpgFileName));
+            } 
+            else if (choice == 5) {
+                ImageIO.write(image, "jpeg", new File(dir.getPath() + jpegFileName));
+            } 
+            else if (choice == 6) {
+                ImageIO.write(image, "png", new File(dir.getPath() + pngFileName));
+                ImageIO.write(image, "gif", new File(dir.getPath() + gifFileName));
+                ImageIO.write(image, "bmp", new File(dir.getPath() + bmpFileName));
+                ImageIO.write(image, "jpg", new File(dir.getPath() + jpgFileName));
+                ImageIO.write(image, "jpeg",new File(dir.getPath() + jpegFileName));
+            } 
+            else {
+                flag=1;
+                System.out.println("Please enter a valid choice");
+            }
+            
+        } catch (Exception e) {
+           System.err.println(e.getMessage());
+        }
+
+        if(flag==0){
+        System.out.println("Done...");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        BufferedImage image=null;
 
         System.out.println("Heyy! What do you Have ?");
         System.out.println("Enter '1' if you have an image URL");
@@ -40,108 +88,82 @@ public class ImageHandling {
         int n = sc.nextInt();
         sc.nextLine();
 
+         // Asking what does the user have a File Path or a URL??
         switch (n) {
             case 1:
                 System.out.println("Please enter the image URL...");
                 String s = sc.nextLine();
-                System.out.println("Please enter the format you want to convert your image File");
-                System.out.println("Please enter 1 for png file format");
-                System.out.println("Please enter 2 for gif file format");
-                System.out.println("Please enter 3 for bmp file format");
-                System.out.println("Please enter 4 for jpg file format");
-                System.out.println("Please enter 5 for jpeg file format");
-                System.out.println("Please enter 6 for all availabale  file format");
-                // System.out.println("Please enter 1 for png file");
-                int choice = sc.nextInt();
-
                 try {
                     URL url = toURL(s);
-                    BufferedImage image = ImageIO.read(url);
-
-                    File dir = new File("./ImageConversion");
-                    if (!dir.exists()) {
-                        dir.mkdirs();
-                    }
-
-                    if (choice == 1) {
-                        ImageIO.write(image, "png", new File(dir.getPath() + "/LAP.png"));
-                    } 
-                    else if (choice == 2) {
-                        ImageIO.write(image, "gif", new File(dir.getPath() + "/LAP.gif"));
-                    } 
-                    else if (choice == 3) {
-                        ImageIO.write(image, "bmp", new File(dir.getPath() + "/LAP.bmp"));
-                    } 
-                    else if (choice == 4) {
-                        ImageIO.write(image, "jpg", new File(dir.getPath() + "/LAP.jpg"));
-                    } 
-                    else if (choice == 5) {
-                        ImageIO.write(image, "jpeg", new File(dir.getPath() + "/LAP.jpeg"));
-                    } 
-                    else if (choice == 6) {
-                        ImageIO.write(image, "png", new File(dir.getPath() + "/LAP.png"));
-                        ImageIO.write(image, "gif", new File(dir.getPath() + "/LAP.gif"));
-                        ImageIO.write(image, "bmp", new File(dir.getPath() + "/LAP.bmp"));
-                        ImageIO.write(image, "jpg", new File(dir.getPath() + "/LAP.jpg"));
-                        ImageIO.write(image, "jpeg", new File(dir.getPath() + "/LAP.jpeg"));
-                    }
-                    else {
-                        System.out.println("Please enter a valid choice");
-                    }
-                    System.out.println("Done....");
-
+                     image = ImageIO.read(url);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
                 break;
 
             case 2:
-
                 System.out.println("Please enter the path of the file");
                 File file = new File(sc.nextLine());
-                System.out.println("Please enter the format you want to convert your image File");
-                System.out.println("Please enter 1 for png file format");
-                System.out.println("Please enter 2 for gif file format");
-                System.out.println("Please enter 3 for bmp file format");
-                System.out.println("Please enter 4 for jpg file format");
-                System.out.println("Please enter 5 for jpeg file format");
-                System.out.println("Please enter 6 for all availabale  file format");
-    
-                int choice1 = sc.nextInt();
-                BufferedImage image = ImageIO.read(file);
-                
-                File dir = new File("./ImageConversion");
-                if (!dir.exists()) {
-                    dir.mkdirs();
-                }
+                 image = ImageIO.read(file);
+                break;
+            
+            default:
+               System.out.println("Please select the valid Number");
 
-                if (choice1 == 1) {
-                    ImageIO.write(image, "png", new File(dir.getPath() + "/LAP.png"));
-                } 
-                else if (choice1 == 2) {
-                    ImageIO.write(image, "gif", new File(dir.getPath() + "/LAP.gif"));
-                } 
-                else if (choice1 == 3) {
-                    ImageIO.write(image, "bmp", new File(dir.getPath() + "/LAP.bmp"));
-                } 
-                else if (choice1 == 4) {
-                    ImageIO.write(image, "jpg", new File(dir.getPath() + "/LAP.jpg"));
-                } 
-                else if (choice1 == 5) {
-                    ImageIO.write(image, "jpeg", new File(dir.getPath() + "/LAP.jpeg"));
-                } 
-                else if (choice1 == 6) {
-                    ImageIO.write(image, "png", new File(dir.getPath() + "/LAP.png"));
-                    ImageIO.write(image, "gif", new File(dir.getPath() + "/LAP.gif"));
-                    ImageIO.write(image, "bmp", new File(dir.getPath() + "/LAP.bmp"));
-                    ImageIO.write(image, "jpg", new File(dir.getPath() + "/LAP.jpg"));
-                    ImageIO.write(image, "jpeg", new File(dir.getPath() + "/LAP.jpeg"));
-                } 
-                else {
-                    System.out.println("Please enter a valid choice");
-                }
-                System.out.println("Done...");
         }
+
+        // Asking the user where he wants to store the file
+        System.out.println("Where do you want to store the file");
+        System.out.println("Please for 1 if you want to store the file in the Downloads");
+        System.out.println("Please for 2 if you want to store the file in the Documents");
+        System.out.println("Please for 3 if you want to store the file in the a paticular path");
+        
+        int filePathChoice=sc.nextInt();
+        sc.nextLine();
+
+        String userHome = System.getProperty("user.home");
+        String documentsPath = userHome + File.separator + "Documents";
+        String downloadsPath = userHome + File.separator + "Downloads";
+
+        System.out.println(documentsPath);
+        
+        String pathname="";
+
+        if(filePathChoice==1){
+            pathname=downloadsPath;
+        }
+        else if(filePathChoice==2){
+            pathname=documentsPath;
+        }
+        else if (filePathChoice==3){
+            System.out.println("Please enter the name of the path you want it get stored at ");
+            pathname=sc.nextLine();
+        }
+
+       // Asking user what name they want to give to their file
+       System.out.println("Please Enter the name of the file");
+       String fileName=sc.nextLine();
+
+
+       // What kind of file format the user wants the current file to be converted into
+        System.out.println("Please enter the format you want to convert your image File");
+        System.out.println("Please enter 1 for png file format");
+        System.out.println("Please enter 2 for gif file format");
+        System.out.println("Please enter 3 for bmp file format");
+        System.out.println("Please enter 4 for jpg file format");
+        System.out.println("Please enter 5 for jpeg file format");
+        System.out.println("Please enter 6 for all availabale  file format");
+
+        // Asking the choice of the user
+        int choice = sc.nextInt();
+        
+        // Creating the Object of the class ImageHandling
+        ImageHandling object=new ImageHandling();
+        
+        // Calling the function ImageConversion
+        object.ImageConversion(image, choice,pathname,fileName);
+
+        sc.close();
 
     }
 }
